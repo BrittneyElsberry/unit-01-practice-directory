7b95521c14e99a580ff8a5593032b40ef1761adc
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-03-13 14:19:53 -0700

    about on auth page

--- unit-07/personal-project-feedback-tool/src/components/Auth/Auth.js
+++ unit-07/personal-project-feedback-tool/src/components/Auth/Auth.js
@@ -19,11 +19,12 @@ const [userInfo, setUserInfo] = useState({
 const [user_admin, setuser_admin] = useState(false)
 const [toggleLogin, setToggleLogin] = useState(false)
 const [toggleRegister, setToggleRegister] = useState(false)
+const [about, setAbout] = useState (false)
 
 
 
 
-const register = ()=>{
+    const register = ()=>{
 
     axios.post('/auth/register', {...userInfo, user_admin})
     .then(res=>{
@@ -34,208 +35,267 @@ const register = ()=>{
 
 }
 
-const login = ()=> {
-axios.post('/auth/login', userInfo)
-.then(user => {
-    props.updateUser(user.data)
-    // setUserInfo(user.data) 
-    props.history.push( props.location.state ? props.location.state.from : '/deptdash')
-})
+    const login = ()=> {
+    axios.post('/auth/login', userInfo)
+    .then(user => {
+        props.updateUser(user.data)
+        // setUserInfo(user.data) 
+        props.history.push( props.location.state ? props.location.state.from : '/deptdash')
+    })
 
-}
-
-
-const displayLoginForm = ()=> {
-setToggleLogin(true)
-setToggleRegister(false)
-
-
-}
-
-const displayRegisterForm = ()=> {
-    setToggleRegister(true)
-    setToggleLogin(false)
     }
+
+
+    const displayLoginForm = ()=> {
+    setToggleLogin(true)
+    setToggleRegister(false)
+    setAbout(false)
+    }
+
+
+    const displayRegisterForm = ()=> {
+        setToggleRegister(true)
+        setToggleLogin(false)
+        setAbout(false)
+
+        }
+        
     
+    const displayAbout = () => {
+        setAbout(!about)
+      
+    }    
 
 
-return (
-
-
-
-<div className='backgroundAuth'>
-
-<div className='employeeEngagement'>
-<p>Welcome to the feedback Hub! <br></br>
-</p>
-<br></br>
-
-<div className='flexauthbtn'>
-<button
-className='authbtn'
-onClick={(e)=>displayLoginForm(e)}
-
->Login</button>
-
-<button
-className='authbtn'
-onClick={(e)=>displayRegisterForm(e)}
-
->Register</button>
-
-</div>
+    return (
 
 
 
+    <div className='backgroundAuth'>
 
-</div>
+                    <div className='employeeEngagement'>
+                    <button className='authbtn' onClick={(e)=>displayAbout(e)}>About</button>
+                        <p>Increase employee engagement with the Feedback Hub! <br></br>
+                        </p>
+                        <br></br>
+                            {/* <button className='aboutbtn' onClick={(e)=>displayAbout(e)}>About</button> */}
+
+                                <div className='flexauthbtn'>
+                                     <button
+                                    className='authbtn'
+                                    onClick={(e)=>displayLoginForm(e)}
+
+                                    >Login</button>
+
+                                    <button
+                                    className='authbtn'
+                                    onClick={(e)=>displayRegisterForm(e)}
+
+                                    >Register</button>
+
+                                </div>
+                                
+                    </div>
 
 
-<div className='authandinfocontainer'>
+        
+                    { about ? 
+                            <div className="aboutus">
+                                <p>
+                                    <li className='border'> Submit and track your feedback history with a personal feedback page.</li>
+                                    <br></br>
+                                    <br></br>
+                                    <li className='border'> Collaborate with leadership through the manager comments feature</li>
+                                    <br></br>
+                                    <br></br>
+                                    {/* <li className='border'>Have a great idea for an internal process or product improvement? Submit your idea and track implementation progress</li>
+                                    <br></br>
+                                    <br></br> */}
+                                    <li className='border'>Submit anonymous feedback that will be tracked by your department with no other identifying data</li>
+                                    <br></br>
+                                    <br></br>
+                                    <li className='border'> At Feedback hub, we believe that every employee has a voice and companies are more productive when employee engagement is high.</li>
+                                    <br></br>
+                                    <br></br>
+                                    <li className='border'> We understand the importance of transparency and accountability from leadership</li>
+                                    
+                                   
+                                </p>
+                                <div className='sideview'>
 
-<div className='info'><p> Provide feedback to your manager about process or product improvements and track the progress of your suggestions!. </p></div>
+                                </div>
 
-{ toggleLogin ? (
+                            </div> 
+                            
+                        : 
+                        null 
+                        
+                    }
+        
+     
 
-<div className='authContainer'>
-<div className='authInputFieldContainer'>
- <p className='labelText'>Username:</p><br></br> 
+               
+
+        
+        
+
+
+
+        <div className='authandinfocontainer'>
+
+
+
+
+                        { toggleLogin ? (
+
+                                <div className='authContainer'>
+                                <div className='authInputFieldContainer'>
+                                <p className='labelText'>Username:</p><br></br> 
+                                    
+                                    <input 
+                                    type='text'
+                                    className='unInput'
+                                    value={userInfo.username}
+                                    onChange={(e)=>setUserInfo({...userInfo, username: e.target.value})}
+
+                                    />
+                                    
+                                    <br></br>
+
+                                    <p className='labelText'>Password:</p><br></br> 
+                                        
+                                        <input 
+                                        type='text'
+                                        className='passInput'
+                                        value={userInfo.password}
+                                        onChange={(e)=>setUserInfo({...userInfo, password: e.target.value}
+                                            
+                                        
+                                        )
+                                        
+                        }
+                                        />
+                        
+                                       
+                        <br></br>
+
+
+                        
+                        
+            
+
+
+                        <br></br>
+
+                                    <div className='authBtnContainer'>
+                                    <button className='authbtn'onClick={login}>Login</button>
+
+                                    </div>
+
+                                    <br></br>
+
+                                    <br></br>
+
+
+                                    </div> 
+                                    </div> 
+                                    )
+                                    :
+                                    <div>
+
+
+                                    </div>
+
+                     }
+
+
+                                { toggleRegister ? 
+
+
+                                <div className='authContainer'>
+                                <div className='authInputFieldContainer'>
+                                <p className='labelText'>Username:</p><br></br> 
+                                    
+                                    <input 
+                                    type='text'
+                                    className='unInput'
+                                    value={userInfo.username}
+                                    onChange={(e)=>setUserInfo({...userInfo, username: e.target.value})}
+
+                                />
+                                <br></br>
+
+                                <p className='labelText'>Password:</p><br></br> 
+                                    
+                                    <input 
+                                    type='text'
+                                    className='passInput'
+                                    value={userInfo.password}
+                                    onChange={(e)=>setUserInfo({...userInfo, password: e.target.value})}
+                                
+                                />
+                                <br></br>
+
+                                <p className='labelText'>Are you a Manager? :</p> 
+                                    
+                                    <input 
+                                    type="checkbox" 
+                                    className="adminCheckbox" 
+                                    checked={user_admin}
+                                    onChange={(e)=>{setuser_admin(e.target.checked)}} /> 
+                                
+                                
+                                <br></br>
+                                <p className='labelText'>Department Number:</p><br></br> 
+                                
+                                    <input 
+                                    type='text'
+                                    className='deptInput'
+                                    value={userInfo.dept_number}
+                                    onChange={(e)=>setUserInfo({...userInfo, dept_number: e.target.value})}
+                                
+                                />
+
+                                <br></br>
+
+                                <div className='authBtnContainer'>
+                                {/* <button className='authbtn'onClick={login}>Login</button> */}
+                                <button className='authbtn' onClick={register}>Register</button>
+                                </div>
+
+                                <br></br>
+
+
+                                </div> 
+                                </div> 
+
+                                : 
+
+                                <div></div>
+
+
+
+                                }
+
+
+                                   
+
+               
+
+                          
+
+
+        </div>
+
+
+                   
+
+
+    </div> 
     
-    <input 
-    type='text'
-    className='unInput'
-    value={userInfo.username}
-    onChange={(e)=>setUserInfo({...userInfo, username: e.target.value})}
-
- />
- <br></br>
-
-<p className='labelText'>Password:</p><br></br> 
     
-    <input 
-    type='text'
-    className='passInput'
-    value={userInfo.password}
-    onChange={(e)=>setUserInfo({...userInfo, password: e.target.value})}
- 
- />
-  <br></br>
-
-{/* <p className='labelText'>Are you a Manager? :</p> 
     
-    <input 
-    type="checkbox" 
-    className="adminCheckbox" 
-    checked={user_admin}
-    onChange={(e)=>{setuser_admin(e.target.checked)}} />  */}
- 
- 
- <br></br>
- {/* <p className='labelText'>Department Number:</p><br></br> 
- 
-    <input 
-    type='text'
-    className='deptInput'
-    value={userInfo.dept_number}
-    onChange={(e)=>setUserInfo({...userInfo, dept_number: e.target.value})} */}
- 
- {/* /> */}
-
-<br></br>
-
-<div className='authBtnContainer'>
-<button className='authbtn'onClick={login}>Login</button>
-{/* <button className='authbtn' onClick={register}>Register</button> */}
-</div>
-
-<br></br>
-{/* <p className='labelText'>Already have an account?</p> */}
-<br></br>
-{/* <button className='authbtn'>Login</button> */}
-
- </div> 
-</div> 
-)
-:
-<div>
-
-
-</div>
-
-}
-
-
-{ toggleRegister ? 
-
-
-<div className='authContainer'>
-<div className='authInputFieldContainer'>
- <p className='labelText'>Username:</p><br></br> 
-    
-    <input 
-    type='text'
-    className='unInput'
-    value={userInfo.username}
-    onChange={(e)=>setUserInfo({...userInfo, username: e.target.value})}
-
- />
- <br></br>
-
-<p className='labelText'>Password:</p><br></br> 
-    
-    <input 
-    type='text'
-    className='passInput'
-    value={userInfo.password}
-    onChange={(e)=>setUserInfo({...userInfo, password: e.target.value})}
- 
- />
-  <br></br>
-
-<p className='labelText'>Are you a Manager? :</p> 
-    
-    <input 
-    type="checkbox" 
-    className="adminCheckbox" 
-    checked={user_admin}
-    onChange={(e)=>{setuser_admin(e.target.checked)}} /> 
- 
- 
- <br></br>
- <p className='labelText'>Department Number:</p><br></br> 
- 
-    <input 
-    type='text'
-    className='deptInput'
-    value={userInfo.dept_number}
-    onChange={(e)=>setUserInfo({...userInfo, dept_number: e.target.value})}
- 
- />
-
-<br></br>
-
-<div className='authBtnContainer'>
-{/* <button className='authbtn'onClick={login}>Login</button> */}
-<button className='authbtn' onClick={register}>Register</button>
-</div>
-
-<br></br>
-
-
- </div> 
-</div> 
-
-: 
-
-<div><h1>testing testing</h1></div>
-
-
-
-}
-
-
-</div>
-</div> )
+    )
 
 
 }
